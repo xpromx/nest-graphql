1afc99b3cc8e8bcf0b03d531e5df7b3e972d8ef3
Rodrigo Ramirez <xpromx@gmail.com>
2022-01-06 01:19:07 +0900

    feat(vercel): deploy try 5

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import serverlessExpress from '@vendia/serverless-express';
 import { Handler } from 'express';
-import { AppModule } from 'src/app.module';
+import { AppModule } from './app.module';
 
 let server: Handler;
 
